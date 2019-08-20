1bfbdd22ea2057f4d10967d7069204e5854a8d5b
dario.zubaray <dzubaray@imagosur.com>
2019-08-20 08:56:23 -0300

    fix barras de progreso. ViewChild y focus del elemento referenciado

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -7,17 +7,35 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class IncrementadorComponent implements OnInit {
 
+  @ViewChild('txtProgress') txtProgreso: ElementRef;
   @Input('nombre') leyenda: string = 'Leyenda';
   @Input() progreso: number = 50;
 
   @Output() cambioValor: EventEmitter<number> = new EventEmitter();
 
-  constructor() {
-    console.log('leyenda: ', this.leyenda);
-    console.log('progreso: ', this.progreso);
-   }
+  constructor() { }
 
   ngOnInit() { }
+
+  onChanges( newValue: number ) {
+    if (!newValue) {
+      newValue = 0;
+    }
+
+    if (newValue >= 100) {
+      this.progreso = 100;
+    } else if (newValue <= 0) {
+      this.progreso = 0;
+    } else {
+      this.progreso = newValue;
+    }
+
+    // const elementHTML: any = document.getElementsByName('progreso');
+    // elementHTML.value = Number( this.progreso );
+    this.txtProgreso.nativeElement.value = this.progreso;
+    this.cambioValor.emit(this.progreso);
+    this.txtProgreso.nativeElement.focus();
+  }
 
   cambiarValor(valor: number) {
     if (this.progreso >= 100 && valor > 0) {
@@ -31,4 +49,5 @@ export class IncrementadorComponent implements OnInit {
     this.progreso = this.progreso + valor;
     this.cambioValor.emit(this.progreso);
   }
+
 }
