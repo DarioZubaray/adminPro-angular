75cba0be320178f8e0e636ee6a4848febdd2a97a
dario zubaray <dzubaray@gmail.com>
2019-09-07 16:01:24 -0300

    configurando el imagen pipe en el header como el sidebar

--- src/app/pipes/imagen.pipe.ts
+++ src/app/pipes/imagen.pipe.ts
@@ -1,12 +1,35 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { URL_SERVICIOS } from '../config/config';
 
 @Pipe({
   name: 'imagen'
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(img: string, tipo: string = 'usuarios'): any {
+    let url = URL_SERVICIOS + '/imagenes';
+    if ( !img ) {
+      return url + '/tipo/imagen-no-encontrada';
+    }
+    if ( img.indexOf('https://') >= 0) {
+      return img;
+    }
+
+    switch (tipo) {
+      case 'usuarios':
+        url += '/usuarios/' + img;
+        break;
+      case 'medicos':
+        url += '/medicos/' + img;
+        break;
+      case 'hospitales':
+        url += 'hospitales/' + img;
+        break;
+      default:
+        console.log('tipo de imagen no existe: ' + tipo);
+        url += '/tipo/tipo-invalido';
+    }
+    return url;
   }
 
 }
