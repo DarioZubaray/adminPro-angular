1dd9f547abdda3ee1e816ac96dd2c935bdc16ee9
dario zubaray <dzubaray@gmail.com>
2019-09-07 18:42:48 -0300

    vista previa de la imagen seleccionada

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { ChartsModule } from 'ng2-charts';
 
@@ -36,6 +37,7 @@ import { ProfileComponent } from './profile/profile.component';
     Graficas1Component
   ],
   imports: [
+    CommonModule,
     SharedModule,
     FormsModule,
     PAGES_ROUTES,
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../../models/usuario.model';
 import { UsuarioService } from '../../services/service.index';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-profile',
@@ -11,6 +12,7 @@ export class ProfileComponent implements OnInit {
 
   usuario: Usuario;
   imagenSubir: File;
+  imagenTemporal: any;
 
   constructor(public _usuarioService: UsuarioService) {
     this.usuario = this._usuarioService.usuario;
@@ -33,7 +35,16 @@ export class ProfileComponent implements OnInit {
       this.imagenSubir = null;
       return;
     }
+
+    if (archivo.type.indexOf('image') < 0) {
+      swal('Solo imagenes', 'El archivo seleccionado no es una imagen', 'error');
+      this.imagenSubir = null;
+    }
+
     this.imagenSubir = archivo;
+    const reader = new FileReader();
+    const urlImagenTemp = reader.readAsDataURL(archivo);
+    reader.onloadend = () => this.imagenTemporal = reader.result;
   }
 
   cambiarImagen() {
