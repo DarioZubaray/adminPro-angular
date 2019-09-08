99219ae248b72c2a2b4443f30aa85c6d5d1266e7
dario zubaray <dzubaray@gmail.com>
2019-09-08 20:48:10 -0300

    componente de medico y servicio basicos

--- src/app/pages/medicos/medicos.component.ts
+++ src/app/pages/medicos/medicos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Medico } from '../../models/medico.model';
+import { MedicoService } from '../../services/medico/medico.service';
 
 @Component({
   selector: 'app-medicos',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicosComponent implements OnInit {
 
-  constructor() { }
+  medicos: Medico[] = [];
+  cargando: boolean = true;
+  totalRegistros: number = 0;
+
+  constructor(public _medicoService: MedicoService) { }
 
   ngOnInit() {
+    this.cargarMedicos();
+    this.totalRegistros = this._medicoService.totalMedicos;
   }
 
+  cargarMedicos() {
+    this.cargando = true;
+
+    setTimeout( () => {
+      this._medicoService.cargarMedicos()
+                         .subscribe( (medicos: any) => {
+                            this.medicos = medicos;
+                            this.cargando = false;
+                         });
+    }, 500);
+  }
+
+  buscarMedico( terminoBusqueda: string) {
+
+  }
+
+  crearMedico() {
+
+  }
+
+  mostrarModal( medico: Medico) {
+
+  }
+
+  guardarMedico( medico: Medico) {
+
+  }
+
+  borrarMedico( medico: Medico) {
+
+  }
 }
--- src/app/services/medico/medico.service.ts
+++ src/app/services/medico/medico.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MedicoService {
 
-  constructor() { }
+  totalMedicos:  number = 0;
+
+  constructor(public http: HttpClient) { }
+
+  cargarMedicos() {
+    const url = URL_SERVICIOS + '/medico';
+    return this.http.get(url)
+                    .pipe(
+                      map( (resp: any) => {
+                        this.totalMedicos = resp.total;
+                        return resp.medicos;
+                      })
+                    );
+  }
 }
