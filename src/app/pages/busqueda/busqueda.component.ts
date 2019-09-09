9440b5da17b2868a5aa761660860eacc5ee30296
dario.zubaray <dzubaray@imagosur.com>
2019-09-09 10:25:32 -0300

    realizando la busqueda de todas las colecciones

--- src/app/pages/busqueda/busqueda.component.ts
+++ src/app/pages/busqueda/busqueda.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
+import { Usuario } from '../../models/usuario.model';
+import { Medico } from '../../models/medico.model';
+import { Hospital } from '../../models/hospital.model';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,9 +13,14 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BusquedaComponent implements OnInit {
 
-  constructor(public activatedRoute:  ActivatedRoute) {
+  usuarios: Usuario[] = [];
+  medicos: Medico[] = [];
+  hospitales: Hospital[] = [];
+
+  constructor(public activatedRoute:  ActivatedRoute, public http: HttpClient) {
     activatedRoute.params.subscribe( parametros => {
       const termino = parametros['termino'];
+      this.buscar(termino);
       console.log(termino);
     });
    }
@@ -18,4 +28,13 @@ export class BusquedaComponent implements OnInit {
   ngOnInit() {
   }
 
+  buscar( termino: string) {
+    const url = URL_SERVICIOS + '/busqueda/todo/' + termino;
+    this.http.get(url).subscribe( (resp: any) => {
+      console.log(resp);
+      this.usuarios = resp.usuarios;
+      this.medicos = resp.medicos;
+      this.hospitales = resp.hospitales;
+    });
+  }
 }
